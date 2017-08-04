52de6553be2e377a7c1b75cadb7fe98ab557b9bb
Daniel Kary <dankary@gmail.com>
2017-08-04 09:12:08 -0400

    template work

--- inst/first-year/2-license-history/1-run-history.R
+++ inst/first-year/2-license-history/1-run-history.R
@@ -2,22 +2,25 @@
 
 # this function runs the parameterized Rmd for different license types
 run_lichist <- function(
-    priv_nm, lic_filter, priv_hist = "NONE", yrs = "2007:2016", test = FALSE, 
+    priv_nm, lic_filter, priv_hist = "NONE", yrs = "2010:2017", test = FALSE, 
     path = "code/2-license-history" # location of by-priv.Rmd (may vary by state)
 ) {
+    if (test) endfile <- "-test.html" else endfile <- ".html"
     rmarkdown::render(file.path(path, "by-priv.Rmd"),
-        params = list(
-            priv_nm = priv_nm, lic_filter = lic_filter, priv_hist = priv_hist, 
-            yrs = yrs,  test = test
-        ), 
-        output_file = paste0("by-priv-", priv_nm, ".html")
+                      params = list(
+                          priv_nm = priv_nm, lic_filter = lic_filter, priv_hist = priv_hist, 
+                          yrs = yrs,  test = test
+                      ), 
+                      output_file = paste0("by-priv-", priv_nm, endfile)
     )
 }
 
 
 # Test --------------------------------------------------------------------
 run_lichist("hunt", 'type %in% c("hunt", "trap", "combo")', test = TRUE)
-run_lichist("small_game", 'priv == "sg"', test = TRUE)
+run_lichist("trout", 'priv == "trout"', test = TRUE)
+run_lichist("saltwater_info_program", 'priv == "sip"', yrs = "2014:2017", test = TRUE)
+run_lichist("combination", 'subtype == "combination"', "all_sports", test = TRUE)
 
 
 # Run Tables -----------------------------------------------------------
@@ -29,15 +32,14 @@ run_lichist("fish", 'type %in% c("fish", "combo")')
 run_lichist("all_sports", 'type %in% c("hunt", "trap", "fish", "combo")')
 
 # Privilege Permissions
-run_lichist("trapping", 'type == "trap"')
-run_lichist("small_game", 'priv == "sg"')
-run_lichist("migratory_bird", 'priv == "mb"')
-run_lichist("firearm_deer", 'priv %in% c("fiream deer", "fiream deer/turkey")')
-run_lichist("firearm_spring_turkey", 'priv %in% c("sp turkey", "fiream deer/turkey")')
-run_lichist("firearm_fall_turkey", 'priv %in% c("fall turkey", "fiream deer/turkey")')
-run_lichist("archery_deer_turkey", 'priv == "bow"')
+run_lichist("saltwater_info_program", 'priv == "sip"', yrs = "2014:2017")
+run_lichist("big_game", 'priv %in% c("bg", "sport")')
+run_lichist("waterfowl", 'priv %in% c("gwc", "sport")')
+run_lichist("trout", 'priv %in% c("trout", "sport")')
+run_lichist("commercial_fishing", 'priv == "comm"')
 
 # Subtype Permissions
-# run_lichist("daily_small_game", 'subtype == "sg daily"', "small_game", test = TRUE)
-run_lichist("daily_small_game", 'subtype == "sg daily"', "small_game")
-run_lichist("sg_and_fish", 'subtype == "sg and fish"', "all_sports")
+run_lichist("combination", 'subtype == "combination"', "all_sports")
+run_lichist("sportsmans", 'subtype == "sportsmans"', "all_sports")
+run_lichist("short_term", 'short_term == "x"', "all_sports")
+run_lichist("nuisance_wildlife", 'subtype == "nuisance"', "hunt")
