0112612bf9d0da37b4184e75afa0c5b5afbe93ad
Daniel Kary <dankary@gmail.com>
2017-08-07 13:28:30 -0400

    dashboard-results templates

--- inst/first-year/4-dashboard-results/1-run-results.R
+++ inst/first-year/4-dashboard-results/1-run-results.R
@@ -1,8 +1,8 @@
 # run all
 
 run_dash <- function(
-    priv_nm, priv_ref = "NONE", yrs = "2007:2016", res_filter = "NONE", res_type = "NONE", 
-    path = "code/6-dashboard-results/by-priv-results.Rmd" # location of Rmd file
+    priv_nm, priv_ref = "NONE", yrs = "2010:2017", res_filter = "NONE", res_type = "NONE", 
+    path = "code/4-dashboard-results/by-priv-results.Rmd" # location of Rmd file
 ) {
     if (!is.null(priv_ref)) is_priv = TRUE else is_priv = FALSE
     output_file = paste0("by-priv-results-", priv_nm, ".html")
@@ -21,32 +21,32 @@ run_dash("hunt")
 run_dash("fish")
 run_dash("all_sports")
 
-# Privs/subtypes that aren't residency-specific
-run_dash("trapping", "hunt")
-run_dash("small_game", "hunt")
-run_dash("migratory_bird", "hunt")
-run_dash("archery_deer_turkey", "hunt")
-run_dash("firearm_deer", "hunt")
-run_dash("firearm_spring_turkey", "hunt")
-run_dash("firearm_fall_turkey", "hunt")
+# Privileges that aren't residency-specific
+run_dash("big_game", "hunt")
+run_dash("waterfowl", "hunt")
+run_dash("trout", "fish")
+run_dash("commercial_fishing", "fish")
+run_dash("saltwater_info_program", "fish")
 
-### Privs/subtypes that are residency-specific
+# Subtypes that aren't residency-specific
+run_dash("combination", "all_sports")
+run_dash("short_term", "all_sports")
+run_dash("nuisance_wildlife", "hunt")
+
+### Residency-specific
 # 1. make reference privs to identify target population (for calculating priv rates)
-run_dash("hunt", res_filter = "nonres")
 run_dash("all_sports", res_filter = "res")
 
 # 2. estimate residency-specific priv results
-run_dash("daily_small_game", "hunt", res_type = "nonres")
-run_dash("sg_and_fish", "all_sports", res_type = "res")
+run_dash("sportsmans", "all_sports", res_type = "res")
 
 
 # Stack Tables for Tableau Input ------------------------------------------
 
-library(dplyr)
-library(tidyr)
+library(tidyverse)
 
 # load
-rdata_dir <- "data/6-dashboard-results"
+rdata_dir <- "data/4-dashboard-results"
 files <- list.files(rdata_dir, pattern = "\\.RDATA$")
 file_paths <- file.path(rdata_dir, files)
 dat <- list()
@@ -57,20 +57,26 @@ for (i in seq_along(file_paths)) {
 dat <- bind_rows(dat)
 
 # note: residency-specific permissions
-filter(dat, group == "daily_small_game", segment == "Residency") %>% View()
-filter(dat, group == "sg_and_fish", segment == "Residency") %>% View()
+filter(dat, group == "sportsmans", segment == "Residency") %>% View()
 
 # quick check
-head(dat)
+head(dat, 10)
 summary(dat)
 count(dat, segment)
 count(dat, group)
 count(dat, metric, segment) %>% spread(segment, n)
-count(dat, group, metric, segment) %>% spread(segment, n) %>% data.frame()
+count(dat, metric, segment, group) %>% spread(segment, n) %>% data.frame()
+
+# provide to Nick for summary
+# counts by year for each group (permission)
+filter(dat, segment == "All", metric == "participants") %>%
+    select(group, year, value) %>%
+    spread(year, value) %>%
+    write.csv(file = "out/priv-counts.csv")
 
 # number of months may very by group-year
 filter(dat, group == "all_sports", segment == "month", 
-       year %in% c(2015, 2015), metric == "participants") %>% View()
+       year == 2016, metric == "participants") %>% View()
 
 # save output
 write.csv(dat, file = "out/dash-out.csv", row.names = FALSE)
