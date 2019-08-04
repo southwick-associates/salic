a44c4ddf5d80e74120921a66d6a4b5bb94390200
Daniel Kary <dankary@gmail.com>
2019-08-04 15:33:21 -0400

    added warn() helper function

--- R/dashboard.R
+++ R/dashboard.R
@@ -1,6 +1,27 @@
 # functions for building dashboard summary data
 
 
-# Prepare Data ------------------------------------------------------------
+# Estimate ------------------------------------------------------------
 
+#' Helper function: print a warning to flag records
+#' 
+#' The warning is only printed if the input data frame has more than one row 
+#' (i.e., only flagged rows should be passed to warn())
+#'
+#' @param flagged data frame: table containing flagged rows
+#' @param msg character: message to be printed in warming
+#' @family dashboard functions
+#' @keywords internal
+#' @export
+#' @importFrom utils capture.output
+#' @examples
+#' x <- data.frame(x = 1:2, val = c("careful", "a bit high"))
+#' warn(x, "I'm a warning")
+warn <- function(flagged, msg) {
+    flagged <- data.frame(flagged)
+    if (nrow(flagged) > 0) {
+        warning(msg, "\n", paste(capture.output(print(flagged)), collapse = "\n"), 
+                call. = FALSE)
+    } 
+}
 
