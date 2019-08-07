c70c76da85a18c85187cf5285e50e44465e49025
Daniel Kary <dankary@gmail.com>
2019-08-07 13:52:52 -0400

    test for scaleup_part()

--- tests/testthat/test-dashboard.R
+++ tests/testthat/test-dashboard.R
@@ -1,8 +1,9 @@
 context("Calculating dashboard metrics")
 library(salic)
+library(dplyr)
+data(history)
 
 test_that("est_part() returns simple count", {
-    data(history)
     
     # overall
     x <- est_part(history, suppress_warning = TRUE, outvar = "var")
@@ -14,4 +15,13 @@ test_that("est_part() returns simple count", {
     y <- dplyr::filter(history, !is.na(R3))
     y <- dplyr::count(y, R3, year)
     expect_equal(x$var, y$n)
+})
+
+test_that("scaleup_part() output sum matches segment_total", {
+    tot <- est_part(history, suppress_warning = TRUE, outvar = "var")
+    x <- est_part(history, "sex", suppress_warning = TRUE, outvar = "var") %>%
+        scaleup_part(tot, outvar = "var") %>% 
+        group_by(year) %>%  
+        summarise(var = sum(var))
+    expect_equal(x$var, tot$var)
 })
